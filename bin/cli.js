2976e7aad778147d4b52f52b140e149a464da8f1
Boris Kubiak <kubiakboris@gmail.com>
2018-09-10 13:15:14 +0200

    Add --banner option

--- bin/cli.js
+++ bin/cli.js
@@ -10,22 +10,25 @@ const cli = meow(`
       $ lyo [options]        Run Lyo
       $ lyo init [options]   Add Lyo to your project
       $ lyo usage [options]  Show how to use the output file 
- 
+
     Options
       --input   -i  Entry file
       --output  -o  Output file / folder
       --name    -n  Module name in browser
+      --banner  -b  Add a banner to the top of the bundle
 
     Examples
       $ lyo
       $ lyo -i main.js
-      $ lyo -n checkThings
+      $ lyo -n runFunction
       $ lyo -o dist/bundle.min.js
+      $ lyo -b 'Lyo\\nLicensed under MIT'
 `, {
 	flags: {
 		input: {type: 'string', alias: 'i'},
 		output: {type: 'string', alias: 'o'},
-		name: {type: 'string', alias: 'n'}
+		name: {type: 'string', alias: 'n'},
+		banner: {type: 'string', alias: 'b'}
 	},
 	description: 'Lyo'
 });
